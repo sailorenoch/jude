f924483884a631975da13ad124bd60a88f64c7a8
sailorenoch <lpsinlife@gmail.com>
2025-12-18 21:06:30 -0600

    k
    0

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-var cacheName = 'legendofbluntfiend';
+var cacheName = 'j';
 
 /* Start the service worker and cache all of the app's content or use the existing one */
 self.addEventListener('install', function (e) {
